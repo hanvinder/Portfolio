6e6ede977a14a99461b0eb89590f756dbe6afc3d
hanvinder <hanvinder@gmail.com>
2016-02-12 22:41:32 -0500

    Added new js file

---
+++ Script/app1.js
@@ -0,0 +1,54 @@
+/*app1.js> Hanvinder Singh Rai> Portfolio> JavaScript for the second (project) page*/
+"use strict";
+(function () {
+	var work;
+
+	work = document.getElementById("work");
+
+	work.innerHTML = " work ";
+
+
+
+})();
+(function () {
+	var project;
+
+	project = document.getElementById("project");
+
+	project.innerHTML = " Projects ";
+
+
+
+})();
+(function () {
+	var firstProject;
+
+	firstProject = document.getElementById("firstProject");
+
+	firstProject.innerHTML = " Font Designed Website <br> A website featuring a short post about a particular font that was designed to be used on the screen. ";
+
+
+
+})();
+
+(function () {
+	var secondProject;
+
+	secondProject = document.getElementById("secondProject");
+
+	secondProject.innerHTML = " Telescope Website <br> A webite with experimental navigation, header, footer, form page and other codes ";
+
+
+
+})();
+
+(function () {
+	var thirdProject;
+
+	thirdProject = document.getElementById("thirdProject");
+
+	thirdProject.innerHTML = "Camera Website <br> A webite with basic use navigation, header, footer, form page and other codes and template with design principles";
+
+
+
+})();

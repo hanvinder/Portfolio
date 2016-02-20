1012351e638b470bfab1c734bcb9b3b8e971e9ff
hanvinder <hanvinder@gmail.com>
2016-02-19 22:34:06 -0500

    js file updated

--- Script/app3.js
+++ Script/app3.js
@@ -1,30 +1,34 @@
+/*app3.js> Hanvinder Singh Rai> Portfolio> JavaScript for the contact page*/
 (function () {
-
-    if (document.getElementById("about") != null) {
-        console.log("About Page");
-    } else if (document.getElementById("project") != null) {
-        console.log("Projects Page");
-    } else if (document.getElementById("contact") != null) {
-        console.log("Contact Page");
-        //declare variables and create a reference to form elements
+	// avoding error from other pages
+	if (document.getElementById("about") != null) {
+		console.log("About Page");
+	}
+	else if (document.getElementById("project") != null) {
+		console.log("Projects Page");
+	}
+	else if (document.getElementById("contact") != null) {
+		console.log("Contact Page");
+		
+		//variable and reference 
 		var Name = document.getElementById("Name");
 		var surname = document.getElementById("surname");
-        var email = document.getElementById("email");
-        var telephone = document.getElementById("telephone");
-        var textarea= document.getElementById("textarea");
-        var submitButton = document.getElementById("submitButton");
-        submitButton.pre
+		var email = document.getElementById("email");
+		var telephone = document.getElementById("telephone");
+		var textarea = document.getElementById("textarea");
+		var submitButton = document.getElementById("submitButton");
+		submitButton.pre
 
-        
-        submitButton.addEventListener("click", function(event){
-            event.preventDefault();
-			 console.log("Name: " + Name.value);
-			  console.log("Surname: " + surname.value);
-            console.log("Email: " + email.value);
-            console.log("Telephone: " + telephone.value);
+		// returning data
+		submitButton.addEventListener("click", function (event) {
+			event.preventDefault();
+			console.log("Name: " + Name.value);
+			console.log("Surname: " + surname.value);
+			console.log("Email: " + email.value);
+			console.log("Telephone: " + telephone.value);
 			console.log("Message: " + textarea.value);
-        });
-        
-        
-    } // end else if
-	})();
+		});
+
+
+	} // end else if
+})();

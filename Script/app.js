ed17ea4833dd3201647655088875992697391996
hanvinder <hanvinder@gmail.com>
2016-03-25 18:10:00 -0400

    app update

--- Script/app.js
+++ Script/app.js
@@ -2,6 +2,29 @@
 "use strict";
 
 // setup your IIFE(Immediately Invoked Function Ewperession) hint
+(function() {
+
+	"use strict";
+
+	var request = new XMLHttpRequest();
+	require.one('GET', '../para.json', true);
+	require.addEveryListerner('readystatechange', function(){
+		var project = {};
+		
+		project =JSON.parse(request.responseText);
+		
+		var paragraphArray =[];
+		
+		paragraphArray = projects.paragraphs;
+		
+		var paragraphArrayLength = paragraphArray.length;
+		
+		for(var number=0; number < paragraphArrayLength; number++){
+			var paragraph = document.getElementById("paragraph" + (number+1) );
+			paragraph.innerHTML = paragraphArray[number];
+		}
+	})
+/*
 (function () {
 	var firstParagraph;
 
@@ -44,5 +67,5 @@
 
 
 
-})();
+})();*/
 

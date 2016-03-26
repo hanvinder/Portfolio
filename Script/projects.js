5bb743ff04a319314c7edee3313e1ba95752fd6b
hanvinder <hanvinder@gmail.com>
2016-03-25 22:14:54 -0400

    added web config

--- Script/projects.js
+++ Script/projects.js
@@ -7,7 +7,7 @@
     request.open('GET', '../projects.json', true);
     request.addEventListener('readystatechange', function() {
         // wait for file to finish loading
-        if (request.readyState === 4 ) {
+        if (request.readyState === 4 && request.status === 200) {
             var projects = {};
 
             // read in the json object
@@ -15,23 +15,27 @@
 
             // declare local paragraph array container
             var paragraphArray = [];
-            
+
             // read in the paragraphs array from the json object
             paragraphArray = projects.paragraphs;
-            
+
             // store length of the paragraphArray into a variable
             var paragraphArrayLength = paragraphArray.length;
-            
+
             // loop through the paragraphArray
-            for(var number=0; number < paragraphArrayLength; number++) {
+            for (var number = 0; number < paragraphArrayLength; number++) {
                 // create a reference to each html paragraph element 
-                var paragraph = document.getElementById("paragraph" + (number+1) );
-                
+                var paragraph = document.getElementById("paragraph" + (number + 1));
+
                 // set the innerHTML of the paragraph to the string from the paragraphArray
                 paragraph.innerHTML = paragraphArray[number];
             }
 
         }
+    });
+    request.send();
+
+})();        }
     });
     request.send();
 

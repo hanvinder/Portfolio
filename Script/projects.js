96e7dae336cdc4267c932d7d4918656b75ed2084
hanvinder <hanvinder@gmail.com>
2016-03-25 21:47:10 -0400

    js file

--- Script/projects.js
+++ Script/projects.js
@@ -7,7 +7,7 @@
     request.open('GET', '../projects.json', true);
     request.addEventListener('readystatechange', function() {
         // wait for file to finish loading
-        if (request.readyState === 4 ) {
+        if (request.readyState === 4 && request.status === 200) {
             var projects = {};
 
             // read in the json object
